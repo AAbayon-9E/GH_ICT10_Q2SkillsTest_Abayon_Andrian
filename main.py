5b0f729c60c21e8adbfebe066589a7f0da7d7f2d
AAbayon-9E <abayon.ajy@obmontessori.edu.ph>
2025-11-27 09:00:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+# yeeeygfywf
+from pyscript import display, document
+
+
+def my_club(e):
+    Club1 = document.getElementById("Art").value
+    Club2 = document.getElementById("Vball").value
+    Club3 = document.getElementById("Book").value
+ 
+
+Club1 = {
+    'Art Club' 
+    'Description':'Art',
+    'Meeting time':'67',
+    'Location':'under there',
+    'Club moderator':'Joe',
+    'Number of members':'67'
+}
+
+def my_club(e):
+    Club2 = document.getElementById("Vball").value
+   
+
+Club2 = {
+    'Volleyball Club' 
+    'Description':'VBalls',
+    'Meeting time':'67',
+    'Location':'under there',
+    'Club moderator':'Joe',
+    'Number of members':'67'
+}
+
+def my_club(e):
+    Club3 = document.getElementById("Book").value
+
+Club3 = {
+    'Book Club' 
+    'Description':'Read',
+    'Meeting time':'67',
+    'Location':'under there',
+    'Club moderator':'Joe',
+    'Number of members':'67'
+}
+
+
+
+
+display((Club1), target='output')
+display((Club2), target='output')
+display((Club3), target='output')
+
